357092023255132b7a7c7a520bfaaf1ec2cf629e
N7Aakash <t2lassi12345@gmail.com>
2019-11-02 15:01:07 +0530

    'tables'

--- views/sections/disease_identify_local.php
+++ views/sections/disease_identify_local.php
@@ -6,14 +6,15 @@ require_once('../includes/header-bp.php');
 ?>
 <section class="about_us single_about_padding"  style="margin: 50px;padding-top: 50px">
   <h1>Upload Image from local Storage : </h1>
-  <form enctype="multipart/form-data" >
+  <div>
     <div class="file-loading">
-        <input id="kv-explorer" type="file" multiple>
+        <input id="kv-explorer" type="file" accept='image/*'>
     </div>
     <br>
-    <button class="btn_2 " type="submit" style="margin-right: 10px;border: 0px"> Submit </button>
+    <img src="" alt="\no image" hidden id="img_element">
+    <button class="btn_2 " type="submit" style="margin-right: 10px;border: 0px" id='btn_submit' onclick="sendImage()"> Submit </button>
     <button class="btn_2 " type="submit" style="margin-right: 10px;border: 0px"> Reset </button>
-  </form>
+  </div>
 </section>
 
 <?php 
@@ -55,3 +56,19 @@ $("#kv-explorer").fileinput({
   ]
 });
 </script>
+
+
+<script async src="https://github.com/Quramy/opencvjs/blob/featuers2d_only/build/cv.js" type="text/javascript"></script>
+<script>
+function sendImage(){
+      var fileInput = document.getElementById('btn_submit');
+      var image = document.getElementById('img_element');
+      fileInput.addEventListener('change',(e)=>{
+        image.src = URL.createObjectURL(e.target.files[0]);
+      },false);
+
+      console.log(cv.imread(image));
+}
+
+</script>
+
